65847058403d82d35ede368e1940ceddb98bd0d3
angel <angelopbernardi13@gmail.com>
2024-09-19 19:55:30 -0300

    Aooo potencia

---
+++ src/view/Principal.java
@@ -0,0 +1,51 @@
+package view;
+
+import model.Autor;
+import model.Emprestimo;
+import model.Livro;
+import model.Usuario;
+
+import javax.swing.*;
+import java.util.jar.JarEntry;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Livro livro = new Livro();
+
+        livro.setId(1);
+        livro.setTitulo("POO: Preguiçosos Observando Objetos");
+        livro.setAutor("Angelo Polli Bernardi");
+        livro.setEditora(" Unisep");
+        livro.setGenero("Romance");
+        livro.setDataPublicacao("14/09/2024");
+
+        Autor autor = new Autor();
+
+        autor.setId(1);
+        autor.setNome(" Angelo Polli Bernardi");
+        autor.setIdade(" 18 Anos");
+        autor.setLivros(" POO: Preguiçosos Observando Objetos | Ctrl+C, Ctrl+V: A Arte do Desenvolvimento| Debugging: Ou Como Culpar o Estagiário ");
+
+        Usuario usuario = new Usuario();
+
+        usuario.setId(1);
+        usuario.setNome(" Ariel Guareschi");
+        usuario.setEmail(" arielguareshi.unisepdv2024@gmail.com");
+        usuario.setCpf(" 666.333.222.11");
+        usuario.setTelefone(" 4002-8922");
+
+        Emprestimo emprestimo = new Emprestimo();
+        emprestimo.setIdEmprestimo(1);
+        emprestimo.setNomeEmprestimo("Ariel Guareschi");
+        emprestimo.setDataEmprestimo("14/09/2024");
+        emprestimo.setHoraEmprestimo("14:33");
+        emprestimo.setDiaRetornoEmprestimo("14/10/2024");
+        emprestimo.setDescEmprestimo(" POO| Preguiçosos Observando Objetos");
+
+        JOptionPane.showMessageDialog(null, emprestimo + livro.toString() + autor.toString() + emprestimo.toString());
+
+    }
+}
+
+
